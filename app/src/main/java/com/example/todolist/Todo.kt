5c5459cf5834dbea194e269c8dcadd696e9f4ada
AndreiVoicea2 <axel26alex@yahoo.com>
2024-04-18 19:09:24 +0300

    First Commit

---
+++ app/src/main/java/com/example/todolist/Todo.kt
@@ -0,0 +1,9 @@
+package com.example.todolist
+
+data class Todo(
+
+    val title : String,
+    val isChecked : Boolean = false
+
+
+)
---
+++ app/src/main/java/com/example/todolist/TodoAdapter.kt
@@ -0,0 +1,17 @@
+package com.example.todolist
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class TodoAdapter(
+
+    private val todos: MutableList<Todo>
+
+) {
+
+    class TodoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+
+
+}
+
