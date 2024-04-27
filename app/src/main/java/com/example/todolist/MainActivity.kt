b99fd531cb18cc717d22fba35cc54e7e3a77dea6
AndreiVoicea2 <axel26alex@yahoo.com>
2024-04-27 21:21:43 +0300

    Refactoring_Update_2

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -11,6 +11,9 @@ import androidx.recyclerview.widget.RecyclerView
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
         val rvTodoItems: RecyclerView = findViewById(R.id.rvTodoItems)
         val todoAdapter = TodoAdapter(mutableListOf())
         val btnAddTodo: Button = findViewById(R.id.btnAddTodo)
@@ -18,9 +21,6 @@ class MainActivity : ComponentActivity() {
         rvTodoItems.adapter = todoAdapter
         rvTodoItems.layoutManager = LinearLayoutManager(this)
 
-
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
 
         btnAddTodo.setOnClickListener{
